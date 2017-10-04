b16ab1325a73c75f699064baea13d77521b059b4
Jonas Laursen <jona8690@edu.eal.dk>
2017-10-04 09:47:28 +0200

    Prepared Public Interface

--- LogDataConversionService/LogDataConversionService/IService.cs
+++ LogDataConversionService/LogDataConversionService/IService.cs
@@ -9,37 +9,41 @@ namespace LogDataConversionService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IService
     {
         [OperationContract]
-        string GetData(int value);
+        string ParseLog(string[] value);
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+		[OperationContract]
+		string ParseLog(string[] value, string Parser);
+
+		//[OperationContract]
+  //      CompositeType GetDataUsingDataContract(CompositeType composite);
 
         // TODO: Add your service operations here
     }
 
     // Use a data contract as illustrated in the sample below to add composite types to service operations.
     // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "LogDataConversionService.ContractType".
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+    //[DataContract]
+    //public class CompositeType
+    //{
+    //    bool boolValue = true;
+    //    string stringValue = "Hello ";
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
+    //    [DataMember]
+    //    public bool BoolValue
+    //    {
+    //        get { return boolValue; }
+    //        set { boolValue = value; }
+    //    }
 
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
+    //    [DataMember]
+    //    public string StringValue
+    //    {
+    //        get { return stringValue; }
+    //        set { stringValue = value; }
+    //    }
+    //}
 }

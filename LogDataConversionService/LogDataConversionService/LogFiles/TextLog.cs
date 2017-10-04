699bb07084c30a60e3d55026009e560a00cf6e66
Jonas Laursen <jona8690@edu.eal.dk>
2017-10-04 10:06:37 +0200

    Moved to proper project type

---
+++ LogDataConversionService/LogDataConversionService/LogFiles/TextLog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDataConversionService.LogFiles
+{
+	public class TextLog : ILogFile
+	{
+		public string Parser { get; set; }
+		private List<string[]> Data;
+
+		public List<string[]> GetData()
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<string> GetHeaders()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

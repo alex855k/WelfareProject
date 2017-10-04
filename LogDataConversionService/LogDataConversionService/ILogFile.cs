3276076252479c2cfb41cfaba76098fd2f240f8d
Jonas Laursen <jona8690@edu.eal.dk>
2017-10-04 09:51:43 +0200

    Made raw log interface

---
+++ LogDataConversionService/LogDataConversionService/ILogFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogDataConversionService
+{
+	interface ILogFile
+	{
+		string Parser { get; }
+
+		List<string> GetHeaders();
+		List<string[]> GetData();
+	}
+}

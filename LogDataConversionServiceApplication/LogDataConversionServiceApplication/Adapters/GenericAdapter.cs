ded003e2fd9fb4bc3f05953ef049531c8a0d5b8f
Jonas Laursen <jona8690@edu.eal.dk>
2017-10-04 11:37:18 +0200

    Added list of headers, for how to parse in the generic adapter

--- LogDataConversionServiceApplication/LogDataConversionServiceApplication/Adapters/GenericAdapter.cs
+++ LogDataConversionServiceApplication/LogDataConversionServiceApplication/Adapters/GenericAdapter.cs
@@ -7,6 +7,15 @@ namespace LogDataConversionServiceApplication.Adapters
 {
 	public class GenericAdapter : LogAdapter
 	{
+		// Colum Headers that determine a DataType
+		List<string> DateHeaders = new List<string> { "Tid" };
+		List<string> NameHeaders = new List<string> { "Navn" };
+		List<string> AlarmHeaders = new List<string> { "Alarm" };
+		List<string> AddressHeaders = new List<string> { "Afdeling" };
+		List<string> PlaceHeaders = new List<string> { "Bolig" };
+		List<string> CallcodeHeaders = new List<string> { "Personale" };
+
+
 		public override List<string[]> ParseData(List<string[]> Data)
 		{
 			throw new NotImplementedException();

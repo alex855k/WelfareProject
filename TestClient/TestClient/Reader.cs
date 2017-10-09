da2dc94d19cecf96de19a453e3a9ac85300bd389
alex855k <alexander2341@gmail.com>
2017-10-09 20:48:21 +0200

    Reader.cs : Refactored a bunch of the code, seperating it into more methods and making it more readable

--- TestClient/TestClient/Reader.cs
+++ TestClient/TestClient/Reader.cs
@@ -14,13 +14,18 @@ namespace TestClient
         Local,URI
     }
 
+    //The responsibility of this class is to read the data and return only the data that is nessecary as a string array.
     public class Reader
     {
-        //The responsibility of this class is to read the data and return only the data that is nessecary as a string array.
         private FileLocation _fileLocationType;
-        private string _fileName;
+
+        public string FilePath => FileDirectory + FileName;
+
+        public string FileName { get; }
+        private string[] _dataFile;
+        private bool _hasHeader = true;
+        private char seperationChar = '\t';
         private readonly char[] specialChars = {'\t', ';'};
-        
         private static string _dateRegexExp = 
                                     // DD-MM-YYYY 
                                     "((?:(?:[0-2]?\\d{1})|(?:[3][01]{1}))[-:\\/.](?:[0]?[1-9]|[1][012])[-"+
@@ -31,12 +36,58 @@ namespace TestClient
                                     "((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])?(?:\\s?(?:am|AM|pm|PM))?)"
                                     ; 
         private readonly Regex _dateRegex = new Regex(_dateRegexExp, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        private int _dateColumnIndex;
 
-        public string LocalDirectory { get; set; }
+        public string FileDirectory { get; set; }
 
-        public Reader(string localDirectory)
+        public Reader(string fileDirectory, string fileName, FileLocation location)
         {
-            LocalDirectory = localDirectory;
+            FileDirectory = fileDirectory;
+            FileName = fileName;
+            InitializeFileToReader();
+        }
+
+        private void InitializeFileToReader()
+        {
+            // Split all lines of data into a string a array
+            _dataFile = File.ReadAllLines(FilePath, Encoding.GetEncoding("iso-8859-1"));
+            // seperation character is stored here, just using a default character
+            seperationChar = GetSeperationChar(_dataFile);
+            _dateColumnIndex = FindDateColumnIndex(_dataFile);
+        }
+
+        private int FindDateColumnIndex(string[] dataFile)
+        {
+            int countLines = 0;
+            foreach (var line in dataFile)
+            {
+                // For every line of data split the data into columns
+                string[] columns = line.Split(seperationChar);
+                // If it's line one try to find the column where the date is located
+                    for (int columnCount = 0; columnCount < columns.Length; columnCount++)
+                    {
+                        Match m = _dateRegex.Match(columns[columnCount]);
+                        if (m.Success)
+                        {
+                            if (countLines == 0) _hasHeader = false;
+                            return columnCount;
+                        }
+                    }
+                }
+            return 0;
+        }
+
+        public char GetSeperationChar(string[] data)
+        {
+            char seperationChar = ';';
+            foreach (var specialC in specialChars)
+            {
+                if (data[0].Contains(specialC))
+                {
+                    seperationChar = specialC;
+                }
+            }
+            return seperationChar;
         }
 
         public string[] GetData(DateTime cutoffDateTime)
@@ -44,62 +95,31 @@ namespace TestClient
             // If file location is still remote fetch a copy and store it locally and set the filepath
             if (_fileLocationType == FileLocation.URI) CopyFileToLocalDirectory();
 
-            if (_filePath != null)
-            {
-                // seperation character is stored here, just using a default character
-                char seperationChar = ' ';
+            if (FilePath != string.Empty) { 
+                         
                 // string array containing the needed strings to send over the network.
                 List<string> parseAbleData = new List<string>();
 
-                // Split all lines of data into a string arr
-                string[] dataFile = File.ReadAllLines(_filePath, Encoding.GetEncoding("iso-8859-1"));
-                int countLines = 0;
-                int dateColumnIndex = 0;
-                foreach (var line in dataFile)
+                foreach (var line in _dataFile)
                 {
-                    foreach (var specialC in specialChars)
+                    // For every line of data split the data into columns
+                    string[] columns = line.Split(seperationChar);
+                    // If date is before the cutoff date
+                    if (DateTime.Compare(Convert.ToDateTime(columns[_dateColumnIndex]), cutoffDateTime) > -1)
                     {
-                        if (line.Contains(specialC))
-                        {
-                            seperationChar = specialC;
-                        }
+                        return parseAbleData.ToArray();
                     }
-                    // OBS might need to be changed, usually logs don't have headers.
-                    if (countLines > 0)
-                    {
-                        // For every line of data split the data into columns
-                        string[] columns = line.Split(seperationChar);
-                        // If it's line one try to find the column where the date is located
-                        if (countLines == 1) {
-                            for (int columnCount = 0; columnCount < columns.Length; columnCount++)
-                            {
-                                Match m = _dateRegex.Match(columns[columnCount]);
-                                if (m.Success)
-                                {
-                                    dateColumnIndex = columnCount;
-                                }
-                            }
-                        }
-
-                        // If date is before the cutoff date
-                        if (DateTime.Compare(Convert.ToDateTime(columns[dateColumnIndex]), cutoffDateTime) > -1)
-                        {
-                            return parseAbleData.ToArray();
-                        }
-                        parseAbleData.Add(columns[dateColumnIndex]);
-                    }
+                    parseAbleData.Add(columns[_dateColumnIndex]);
                 }
             }
             throw new Exception("File not located!");
         }
 
-        public string _filePath { get; private set; }
-
         private void CopyFileToLocalDirectory()
         {
             //throw new NotImplementedException();
             // Implement this yourselves
-            _fileLocationType = FileLocation.Local;
+
             // Use webclient to grab file for instance.
             using (var client = new WebClient())
             {

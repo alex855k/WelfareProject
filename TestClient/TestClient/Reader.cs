f08327020c3e7de8073f055b734986b6f7c1bd5e
alex855k <alexander2341@gmail.com>
2017-10-09 16:15:31 +0200

    started implementing some methods for the reader, need to add some regex expressions to identify column with date

--- TestClient/TestClient/Reader.cs
+++ TestClient/TestClient/Reader.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Threading;
 using System.Web;
 using System.Windows;
 
@@ -35,19 +36,47 @@ namespace TestClient
             
         }
 
-        public string[] GetNewData()
+        public string[] GetData()
         {
             // If file location is still remote fetch a copy and store it locally and set the filepath
             if (_fileLocationType == FileLocation.URI) CopyFileToLocalDirectory();
 
             if (_filePath != null)
             {
-                string[] data = File.ReadAllLines(_filePath, Encoding.GetEncoding("iso-8859-1"));
+                char[] specialChars = { '\t', ';' };
+                
 
-                foreach (var l in data)
+
+                // Split all lines of data into a string arr
+                string[] data = File.ReadAllLines(_filePath, Encoding.GetEncoding("iso-8859-1"));
+                int countLines = 0;
+                foreach (var line in data)
                 {
-                    string[] arr = l.Split('\t');
-                    if (arr) { }
+                    if (countLines == 1)
+                    {
+                        //line.
+                    }
+                    // Looks for special character either horizontal tab hex09 or semi-colon hex3B
+                    char seperationChar;
+                    
+
+
+                    // For every line of data split the data into columns
+                    string[] columns = line.Split('\t');
+                    foreach (var col in columns)
+                    {
+                        //check column for location of datetime data then store location
+                        char[] columnCharArr = col.ToCharArray();
+                        foreach (char c in columnCharArr)
+                        {
+
+                        }
+                        if ()
+                        {
+                            
+                        }
+                    }
+                countLines++;
                 }
 
 
